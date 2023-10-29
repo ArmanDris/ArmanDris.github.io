429dd7f097d47ff1f56bea17fde1ad7e6dc308dc
Arman Drismir <adrismir@gmail.com>
2023-10-29 01:42:25 -0700

    Reverted to Sudoku

---
+++ deliveryboy.js
@@ -0,0 +1,54 @@
+class DeliveryBoy {
+  deliver(name, score) {
+    const url = `http://127.0.0.1:5000/mailbox`;
+
+    fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-store',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ name: name, score: score })
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Score sent successfully');
+          return true;
+        }
+        else {
+          console.error('Failed to send score, Error:', response.status);
+        }
+      })
+      .catch(error => {
+        console.error('Error', error)
+      })
+  }
+
+  async receive() {
+    try {
+      const url = `http://127.0.0.1:5000/leaderboard`;
+
+      const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-store',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        return data.leaderboard;
+      }
+      else {
+        throw new Error(response.status);
+      }
+    }
+    catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+}
