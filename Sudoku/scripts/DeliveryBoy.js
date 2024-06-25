d4c62ee98ecd9fb41269203939153744457d6504
armandrismir <adrismir@outlook.com>
2024-06-24 17:17:21 -0700

    Update sudoku

--- Sudoku/scripts/DeliveryBoy.js
+++ Sudoku/scripts/DeliveryBoy.js
@@ -1,27 +1,9 @@
 class DeliveryBoy {
-	public_url = 'https://23.16.237.88';
-	local_url = 'https://cors-anywhere.herokuapp.com/https://23.16.237.88';
-	// Get access to demo server at: https://cors-anywhere.herokuapp.com/corsdemo
-
-	url;
-
-	async setURL() 
-	{
-		try {
-			const response = await fetch(this.public_url + '/ping');
-			if (response.ok)
-				this.url = this.public_url;
-		} catch (error) {
-			this.url = this.local_url;
-		}
-	}
+	server = 'http://strawberrypie.drismir.ca';
 
 	async deliver(name, time, difficulty) {
 
-		if (!this.url)
-			await this.setURL();
-
-		fetch(this.url + '/mailbox', {
+		fetch(this.server + '/mailbox', {
 			method: 'POST',
 			mode: 'cors',
 			cache: 'no-store',
@@ -46,11 +28,8 @@ class DeliveryBoy {
 
 	async receive() {
 
-		if (!this.url) 
-			await this.setURL();
-
 		try {
-			const response = await fetch(this.url + '/leaderboard', {
+			const response = await fetch(this.server + '/leaderboard', {
 				method: 'POST',
 				mode: 'cors',
 				cache: 'no-store',
